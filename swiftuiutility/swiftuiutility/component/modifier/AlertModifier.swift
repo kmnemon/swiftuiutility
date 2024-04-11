68952ce690c3be7ece7f67eeb9836dd9fca1d4bd
kmnemon <kmnemon@outlook.com>
2024-04-11 08:10:15 +0800

    add more on alertmodifier

--- swiftuiutility/swiftuiutility/component/modifier/AlertModifier.swift
+++ swiftuiutility/swiftuiutility/component/modifier/AlertModifier.swift
@@ -7,14 +7,23 @@
 
 import SwiftUI
 
+extension AlertModifier {
+    struct User {
+        var name: String
+    }
+}
+
 struct AlertModifier: View {
     @State private var showingAlert = false
+    
+    @State private var user: User?
+    @State private var showingAlert2 = false
     
     var body: some View {
         Button("show alert"){
             showingAlert = true
         }
-        .alert("Important message", isPresented: $showingAlert){
+        .alert("Important message", isPresented: $showingAlert) {
             Button("OK"){}
             Button("Continue", action: { () ->Void in })
             Button("Delete", role: .destructive){ 
@@ -23,7 +32,15 @@ struct AlertModifier: View {
             Button("Cancel", role: .cancel){}
         } message: {
             Text("Please read this")
-        }        
+        }   
+        
+        Button("binding to an optional") {
+            showingAlert2 = true
+        }
+        .alert(user?.name ?? "ke", isPresented: $showingAlert2, presenting: user) { _ in
+        } message: { u in
+            Text(u.name)
+        }
     }
 }
 
