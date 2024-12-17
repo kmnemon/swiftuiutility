2207fd6c968a4cccc760646a207067cdb78587ef
kmnemon <kmnemon@outlook.com>
2024-12-17 17:11:57 +0800

    view that fit view

--- swiftuiutility/swiftuiutility/component/layout/ViewThatFitsView.swift
+++ swiftuiutility/swiftuiutility/component/layout/ViewThatFitsView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 /*
  When we want to display diﬀerent views depending on the proposed size, we can use
  ViewThatFits. It takes a number of subviews, and it displays the first subview that fits.
+ It does so by proposing nil to figure out the ideal size for each subview, and it displays
+ the first subviews (in the order the subviews appear in the code) where the ideal size
+ fits within the proposed size. If none of the subviews fit, it picks the last subview
  */
 
 struct ViewThatFitsView: View {
