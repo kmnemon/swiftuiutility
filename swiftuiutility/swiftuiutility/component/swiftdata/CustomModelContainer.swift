ce17a3754fd12626e60fe9dec36058e5e91cc813
kmnemon <kmnemon@outlook.com>
2025-01-20 11:41:49 +0800

    custom swiftdata

---
+++ swiftuiutility/swiftuiutility/component/swiftdata/CustomModelContainer.swift
@@ -0,0 +1,33 @@
+//
+//  CustomModelContainer.swift
+//  swiftuiutility
+//
+//  Created by ke on 2025/1/20.
+//
+
+import SwiftData
+
+/*
+@main
+struct CustomModelContainer: App {
+    var container: ModelContainer
+    
+    init() {
+        do {
+            let storeURL = URL.documentsDirectory.appending(path: "database.sqlite")
+            let config = ModelConfiguration(url: storeURL)
+            container = try ModelContainer(for: Recipe.self, configurations: config)
+        } catch {
+            fatalError("Failed to configure SwiftData container.")
+        }
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(container)
+    }
+}
+
+*/
--- swiftuiutility/swiftuiutility/component/swiftdata/book/SwiftDataView.swift
+++ swiftuiutility/swiftuiutility/component/swiftdata/book/SwiftDataView.swift
@@ -11,6 +11,12 @@ import SwiftData
 //Step 2: In swiftuittilityApp define .modelContainer(automate produce modelContext)
 //Step 3: Using @Query and modelContext
 
+/*
+ 1. ModelContainer - is responsible for creating and managing the actual database file used for all SwiftData’s storage needs.
+ 2. ModelContext - has the job of tracking all objects that have been created, modified, and deleted in memory, so they can all be saved to the model container at some later point.
+ 3. ModelConfiguration - determines how and where data is stored, including which CloudKit container to use if any, and whether saving should be enabled or not. This configuration is provided to your model container to determine how it behaves.
+ */
+
 
 
 struct SwiftDataView: View {
