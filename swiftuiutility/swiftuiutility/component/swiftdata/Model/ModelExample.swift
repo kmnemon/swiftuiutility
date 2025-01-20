676f03851d73eb377d5108b689aef0e729b7bec1
kmnemon <kmnemon@outlook.com>
2025-01-20 18:43:06 +0800

    model

--- swiftuiutility/swiftuiutility/component/swiftdata/Model/ModelExample.swift
+++ swiftuiutility/swiftuiutility/component/swiftdata/Model/ModelExample.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftData
+import Foundation
 
 //example 1: simple model
 @Model
@@ -49,7 +50,6 @@ class EmployeeA2 {
 3. We create a second credit card using the number 1234.
 4. We set the balance of that to be $0.
 SwiftData will not allow us to have two credit cards with the same number, because we marked that attribute as unique, so instead when that code finishes we’ll have just one card with a balance of $0 – the most recent value.
- 
  */
 
 
@@ -64,3 +64,51 @@ class EmployeeA3 {
         self.emailAddress = emailAddress
     }
 }
+
+//example 4: add special attributes - transient
+/*
+ the levelsPlayed starts with a default value of 0 when your app runs, and will automatically reset to 0 when the app terminates – it won’t be stored inside SwiftData.
+ There are two important things to remember when working with transient properties in SwiftData:
+ 1.Transient properties cannot be used in SwiftData query predicates because the actual data you’re working with doesn’t exist on disk. Attempting to use a transient property will compile just fine, but crash at runtime.
+ 2.Transient properties must always have a default value as shown above, so that when you create a new object or when an existing one is fetched from disk, there’s always a value present.
+ */
+@Model
+class PlayerA1 {
+    var name: String
+    var score: Int
+    @Transient var levelsPlayed = 0
+
+    init(name: String, score: Int) {
+        self.name = name
+        self.score = score
+    }
+}
+
+//example 5: store swiftdata attributes in an external file
+@Model
+class UserA3 {
+    var name: String
+    var score: Int
+    @Attribute(.externalStorage) var avatar: Data
+
+    init(name: String, score: Int, avatar: Data) {
+        self.name = name
+        self.score = score
+        self.avatar = avatar
+    }
+}
+
+//example 6: store swiftdata attributes in encryption way
+@Model
+class UserA4 {
+    var name: String
+    var score: Int
+    @Attribute(.externalStorage, .allowsCloudEncryption) var avatar: Data
+
+    init(name: String, score: Int, avatar: Data) {
+        self.name = name
+        self.score = score
+        self.avatar = avatar
+    }
+}
+
