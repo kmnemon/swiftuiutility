a059b4e4863a410ec9621fe30449e7ac377a3a35
kmnemon <kmnemon@outlook.com>
2025-02-27 14:24:35 +0800

    fixedsize update

--- swiftuiutility/swiftuiutility/component/modifier/layoutmodifier/FixedSizeModifier.swift
+++ swiftuiutility/swiftuiutility/component/modifier/layoutmodifier/FixedSizeModifier.swift
@@ -10,11 +10,28 @@ import SwiftUI
 
 struct FixedSizeModifier: View {
     var body: some View {
-        VStack {
-            Text("Hello World!")
-                .fixedSize()
+        //Example 1: Text warp
+        Text("A single line of text, too long to fit in a box.")
+            .frame(width: 200, height: 200)
+            .border(Color.gray)
+        
+        //maintain the ideal size of its children ie.Text()
+        Text("A single line of text, too long to fit in a box.")
+            .fixedSize()
+            .frame(width: 200, height: 200)
+            .border(Color.gray)
+        
+        //Example 2: make two text views have the same height
+        HStack {
+            Text("Forecast")
+                .padding()
+                .frame(maxHeight: .infinity)
+                .background(.yellow)
+            Text("The rain in Spain falls mainly on the Spaniards")
+                .padding()
+                .frame(maxHeight: .infinity)
+                .background(.cyan)
         }
-        .frame(width: 30)
-        .border(.blue)
+        .fixedSize(horizontal: false, vertical: true)
     }
 }
