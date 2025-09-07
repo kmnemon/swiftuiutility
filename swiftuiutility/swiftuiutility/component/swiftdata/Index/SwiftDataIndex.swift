27fdabd6203515ace7e50cfc78c1914c9f2e30ba
kmnemon <kmnemon@outlook.com>
2025-09-07 12:56:15 +0700

    index

---
+++ swiftuiutility/swiftuiutility/component/swiftdata/Index/SwiftDataIndex.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftDataIndex.swift
+//  swiftuiutility
+//
+//  Created by ke Liu on 9/7/25.
+//
+import SwiftData
+import Foundation
+
+@Model final class Item {
+    #Index<Item>([\.name], [\.timestamp], [\.name, \.timestamp])
+    
+  var name: String
+  var timestamp: Date
+
+  init(name: String, timestamp: Date) {
+    self.name = name
+    self.timestamp = timestamp
+  }
+}
