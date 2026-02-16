26cc726d3da0b459a20373bd1badc362386c411a
kmnemon <kmnemon@outlook.com>
2026-02-16 07:50:27 +0700

    adjust

--- swiftuiutility/swiftuiutility/component/layout/GeometryReader/GeometryReaderViewWithAncestor.swift
+++ swiftuiutility/swiftuiutility/component/layout/GeometryReader/GeometryReaderViewWithAncestor.swift
@@ -10,17 +10,9 @@ import SwiftUI
 //example: measure the size of a single view and propagate the measurement to an ancestor
 //measure the text proposed size and using onChange propagate tooWide to the .border ancestor view
 
-//helper function
-extension View {
-    func onAppearOrChange<Value: Equatable>(of value: Value, perform: @escaping (Value) -> ()) -> some View
-    {
-        self
-//            .onAppear { perform(value) }
-            .onChange(of: value, initial: true) { perform(value) }
-    }
-}
 
-struct GeometryReaderView7: View {
+
+struct GeometryReaderViewWithAncestor: View {
     var text: String
     @State private var overflows: Bool = false
     
@@ -45,3 +37,34 @@ struct GeometryReaderView7: View {
     }
 }
 
+//add helper function, not necessary in iOS 17
+extension View {
+    func onAppearOrChange<Value: Equatable>(of value: Value, perform: @escaping (Value) -> ()) -> some View
+    {
+        self
+//            .onAppear { perform(value) }
+            .onChange(of: value, initial: true) { perform(value) }
+    }
+}
+
+struct GeometryReaderViewWithAncestor2: View {
+    var text: String
+    @State private var overflows: Bool = false
+    
+    var body: some View {
+        Text(text)
+            .fixedSize()
+            .overlay {
+                GeometryReader { proxy in
+                    let tooWide = proxy.size.width > 100
+                    Color.clear
+                        .onAppearOrChange(of: tooWide) { value in
+                            overflows = value
+                        }
+                }
+                
+            }
+            .frame(width: 100)
+            .border(overflows ? Color.red : Color.clear)
+    }
+}
